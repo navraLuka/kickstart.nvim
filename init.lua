db37cc7157a5c99d124841499ab159b064957a3d
Lukas <xnavral00@stud.fit.vut.cz>
2025-04-14 14:07:36 +0200

    Added formaters

--- init.lua
+++ init.lua
@@ -764,11 +764,15 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        bash = { 'shfmt' },
+        html = { 'html_beautify' },
+        css = { 'css_beautify' },
+        -- js = { 'js_beautify' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
